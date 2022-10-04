bcff80dde9be2aa7a127daa23571d40f486137f7
alanliu2009 <alanliu2009@gmail.com>
2022-10-04 17:10:26 -0400

    Delete JARVA directory

--- JARVA/src/engine/Values.java
+++
@@ -1,6 +0,0 @@
-package engine;
-
-public class Values {
-	public static float Default_Stun_Timer = 0.01f; // Default Stun Timer
-	public static float Default_Invulnerability_Timer = 0.025f; // Default Invulnerability Timer
-}
--- JARVA/src/ui/display/animation/components.java
+++
@@ -1,5 +0,0 @@
-package ui.display.animation;
-
-class components {
-
-}
